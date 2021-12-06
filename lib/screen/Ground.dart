792437e32b753c93c6293db666a771f7e3afb483
shsh <rypush@naver.com>
2021-12-06 16:51:49 +0900

    firestore gridview

--- lib/screen/Ground.dart
+++ lib/screen/Ground.dart
@@ -1,10 +1,12 @@
 import 'dart:collection';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:chips_choice_null_safety/chips_choice_null_safety.dart';
 import 'package:get/get.dart';
+import 'package:sportchu/screen/test.dart';
 
 class ground extends StatefulWidget {
   const ground({Key? key}) : super(key: key);
@@ -14,13 +16,12 @@ class ground extends StatefulWidget {
 }
 
 class _groundState extends State<ground> {
-  CollectionReference firestore = FirebaseFirestore.instance.collection(
-      'category');
-  Stream<QuerySnapshot> cs = FirebaseFirestore.instance.collection('category')
-      .snapshots();
+  CollectionReference firestore =
+      FirebaseFirestore.instance.collection('category');
   String ex = '';
   int tag1 = 0;
   int tag2 = 0;
+  List<String> firelist = [];
   List<String> cateout = [
     'all',
     '축구장',
@@ -32,42 +33,29 @@ class _groundState extends State<ground> {
     '농구장'
   ];
 
-  List<Container> gridtile(int count) {
-    return List.generate(
-      count,
-          (index) =>
-          Container(
-            child: Column(
-              children: [
-                FlutterLogo(),
-                Text(
-                  cateout[index],
-                  style: GoogleFonts.lato(backgroundColor: Colors.teal),
-                ),
-              ],
-            ),
-          ),
-    );
-  }
-
-  ListTile _tile(String title) =>
-      ListTile(
+  ListTile _tile(String title) => ListTile(
         title: Text(
           title,
           style: GoogleFonts.lato(color: Colors.black, fontSize: 20),
         ),
       );
 
-  // @override
-  // void initState() async {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   int i = 0;
-  //   var ca = await firestore.collection('category').get();
-  //   print('1111111111111111$ca');
-  //
-  // }
+  Future<void> getfirelist() async {
+    await firestore.snapshots().listen((event) {
+      event.docs.forEach((element) {
+        print(element['name']);
+        firelist.add(element['name']);
+      });
+    });
+  }
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getfirelist();
+    print(firelist.length);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,10 +80,9 @@ class _groundState extends State<ground> {
                 ),
                 ChipsChoice<int>.single(
                   value: tag1,
-                  onChanged: (val) =>
-                      setState(() {
-                        tag1 = val;
-                      }),
+                  onChanged: (val) => setState(() {
+                    tag1 = val;
+                  }),
                   choiceItems: C2Choice.listFrom<int, String>(
                       source: ['실외', '실내'],
                       value: (i, v) => i,
@@ -106,12 +93,12 @@ class _groundState extends State<ground> {
                   ),
                   wrapped: true, //진실은 여러줄 거짓은 한줄
                 ),
+
                 ChipsChoice<int>.single(
                   value: tag2,
-                  onChanged: (val) =>
-                      setState(() {
-                        tag2 = val;
-                      }),
+                  onChanged: (val) => setState(() {
+                    tag2 = val;
+                  }),
                   choiceItems: C2Choice.listFrom<int, String>(
                       source: _cate(tag1),
                       value: (i, v) => i,
@@ -124,31 +111,67 @@ class _groundState extends State<ground> {
                 ),
 
                 Text(tag1.toString() + Get.width.toString()),
+
                 Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: TextFormField(
                     style: GoogleFonts.lato(
-                        color: Colors.pinkAccent,
-                        fontSize: 16
-                    ),
+                        color: Colors.pinkAccent, fontSize: 16),
                     onChanged: (value) => ex = value,
                   ),
                 ),
-                ElevatedButton(onPressed: () async {
-                  if (ex != '') {
-                    firestore.doc(ex).set({'inout': 0, 'name': '$ex'});
-                  }
-                  var doc = await firestore.doc(ex).get();
-                  print(doc.data());
-                }, child: Text('add')),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (ex != '') {
+                        firestore.doc(ex).set({'inout': 1, 'name': '$ex'});
+                      }
+                      var doc = await firestore.doc(ex).get();
+                      print(doc.data());
+                    },
+                    child: Text('add')),
+                ElevatedButton(
+                    onPressed: () async {
+                      firestore.doc(ex).update({'inout': 1, 'name': '$ex'});
+                      // firestore.doc(ex).delete();
+                    },
+                    child: Text('col')),
 
                 Expanded(
-                  child: GridView.extent(
-                    maxCrossAxisExtent: Get.width / 2,
-                    padding: const EdgeInsets.all(4),
-                    children: gridtile(cateout.length),
-                  ),
-                ),
+                    child: StreamBuilder(
+                        stream: firestore.snapshots(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (!snapshot.hasData) {
+                            return Container();
+                          } else {
+                            return GridView.builder(
+                              padding: const EdgeInsets.all(4),
+                              itemCount: firelist.length,
+                              itemBuilder: (BuildContext context, int index) => Container(
+                                child: GestureDetector(
+                                  onTap: (){
+                                    setState(() {
+                                      Get.to(test(), arguments: firelist[index]);
+                                    });
+                                  },
+                                  child: Column(
+                                    children: [
+                                      FlutterLogo(),
+                                      Text(
+                                        firelist[index],
+                                        style: GoogleFonts.lato(backgroundColor: Colors.yellow),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                            );
+                          }
+                        })
+
+                    ),
                 // ListView(),
               ],
             ),
@@ -169,4 +192,11 @@ class _groundState extends State<ground> {
     }
     return b;
   }
+
+// List<Container> gridtile(int count) {
+//   return List.generate(
+//     count,
+//     (index) =>
+//   );
+// }
 }
